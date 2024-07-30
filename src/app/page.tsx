55bd506817157b55b4fac5e52c42875ce4ac1189
Subash Jirel <subashjrl@gmail.com>
2024-07-30 11:22:15 +0545

    added navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -103,7 +103,7 @@ export default function Home() {
                 src="/line.png"
                 className="absolute w-20 -left-6 -bottom-6 select-none"
               />
-              <Phone className="w-64" imgSrc="/testimonials/justin.jpg" />
+              <Phone className="w-64" imgSrc="/testimonials/justin2.jpg" />
             </div>
           </div>
         </MaxWidthWrapper>
