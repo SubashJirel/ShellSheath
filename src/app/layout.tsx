0e7c9a5bf5d0d6a680b936f3f7e26e8d73525f07
Subash Jirel <subashjrl@gmail.com>
2024-08-01 15:02:37 +0545

    the picture is succesfully placed in the uploadThing server

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import { Recursive } from 'next/font/google';
+import './globals.css';
+import Navbar from '@/components/Navbar';
+import { Toaster } from '@/components/ui/toaster';
+
+const recursive = Recursive({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={recursive.className}>
+        <Navbar />
+
+        <main className="flex grainy-light flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">{children}</div>
+        </main>
+
+        <Toaster />
+      </body>
+    </html>
+  );
+}
